87104a521022e7b8c90b913c726f9bc6a80baeef
simpleY <qingyiweb@foxmail.com>
2023-04-05 16:38:39 +0800

    ci: move step of building to `scripts/build.sh`

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+BASE_DIR=$(pwd)
+OUTPUT_DIR=$BASE_DIR/outputs
+TARGETS=("win-x86" "win-x64" "linux-arm" "linux-arm64" "linux-x64")
+
+# Prepare
+mkdir $OUTPUT_DIR
+echo "Output directory: $OUTPUT_DIR"
+
+# Build
+dotnet restore
+for TARGET in ${TARGETS[@]}; do
+  echo "Build for $target"
+  TARGET_OUTPUT_DIR=$OUTPUT_DIR/$TARGET
+  mkdir $TARGET_OUTPUT_DIR
+  dotnet publish -r $TARGET -o $xTARGET_OUTPUT_DIR -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true --self-contained
+done
+
+
